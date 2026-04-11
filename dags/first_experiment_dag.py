9e5244ed1103438747306b6d731f91001db05808
Akshay muke <akshaymuke91@gmail.com>
2026-04-11 21:40:59 +0530

    Added first tryout dag

---
+++ dags/first_experiment_dag.py
@@ -0,0 +1,32 @@
+from airflow.sdk import dag, task
+
+@dag(
+    dag_id="my_first_dag"
+)
+def first_dag_tryout():
+
+    @task.python
+    def first_task():
+        print("this is first to execute")
+    
+    @task.python
+    def second_task():
+        print("this is second to execute")
+
+    @task.python
+    def third_task():
+        print("this is third to execute")
+
+    @task.python
+    def fourth_task():
+        print("this is last task to execute")
+
+    first = first_task()
+    second = second_task()
+    third = third_task()
+    fourth = fourth_task()
+
+    first >> second >> third >> fourth
+
+
+first_dag_tryout()
